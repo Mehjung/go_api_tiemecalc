e9817e4760c872d1a874c2a8b40c854d13a7a0aa
Mehjung <89900707+Mehjung@users.noreply.github.com>
2024-05-03 21:21:25 +0000

    added endpoint test

--- main.go
+++ main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"fmt"
 	"go_api_timecalc/timemodule"
+	"net/http"
+	"sync"
 )
 
 func main() {
-    sp := timemodule.NewDefaultStringProcessor()
-    result, err := sp.ProcessString("7.48 + 6.30 - ( 5.20)")
-    if err != nil {
-        fmt.Println("Fehler beim Verarbeiten des Strings:", err)
-        return
-    }
+    http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
+        var wg sync.WaitGroup
+        wg.Add(1)
 
-    fmt.Println("Ergebnis:", result)
+        go func() {
+            defer wg.Done()
+
+            sp := timemodule.NewDefaultStringProcessor()
+            result, err := sp.ProcessString("7.48 + 6.30 - ( 5.20)")
+            if err != nil {
+                http.Error(w, "Fehler beim Verarbeiten des Strings: "+err.Error(), http.StatusInternalServerError)
+                return
+            }
+            fmt.Fprintf(w, "Ergebnis: %v", result)
+        }()
+
+        wg.Wait()
+    })
+
+    fmt.Println("Server läuft auf Port 8080")
+    http.ListenAndServe(":8080", nil)
 }
